ff4338fda3edaec84b0a3783fa4f68e2556c21e6
Braxton Gunter <braxton.gun@gmail.com>
2018-04-12 18:09:39 -0400

    prime scheduler and event listeners on dates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,9 @@
 function ready() {
+  calendar_init();
+  schedule_init();
+}
+
+function calendar_init() {
   // load half-a-year forward and back
   var calendars = document.getElementById('calendars');
   for (var i=-6; i<=6; i++) {
@@ -12,6 +17,13 @@ function ready() {
 
   var scrollMax = calendars.scrollWidth - calendars.clientWidth;
   calendars.scrollLeft = scrollMax / 2;
+}
+
+function schedule_init() {
+  var today = document.getElementById('today');
+  var scheduleDate = document.getElementById('schedule-date'); 
+
+  scheduleDate.innerText = `${today.dataset.day}, ${today.dataset.month} ${today.dataset.date} ${today.dataset.year}`;
 }
 
 document.onLoad = ready();
