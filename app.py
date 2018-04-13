c5adcf8b90d2ba11a5967007c1d95a95369a2d68
janukem <janukem@due.com>
2018-04-13 11:16:56 -0400

    finished updating app.py

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from sqlalchemy import *
 from sqlalchemy.pool import NullPool
-from flask import Flask, request, render_template, g, redirect, Response
+from flask import Flask, request, g, redirect, Response
+from flask.ext.mako import MakoTemplates, render_template
+from plim import preprocessor
 
+# Mako so that we can use Plim processor 
 app = Flask(__name__)
+mako = MakoTemplates(app)
+app.config['MAKO_PREPROCESSOR'] = preprocessor
 
 # global vars for accessing DB 
 DATABASEURI = "postgresql://jea2161:1955@35.227.79.146/proj1part2"
@@ -19,7 +24,7 @@ except:
 
 @app.route("/")
 def login():
-	return render_template("login.html")
+	return render_template("login.slim")
 
 if __name__ == "__main__":
 	app.run(debug=True)
