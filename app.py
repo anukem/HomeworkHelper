56ba572ea04ff24d0c5446739b7350823a7a26cf
janukem <janukem@due.com>
2018-04-13 02:09:03 -0400

    Changed app.py and added requirements

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask 
-from flask import render_template
+from sqlalchemy import *
+from sqlalchemy.pool import NullPool
+from flask import Flask, request, render_template, g, redirect, Response
+
 app = Flask(__name__)
+DATABASEURI = "postgresql://jea2161:1955@35.227.79.146/proj1part2"
+
+engine = create_engine(DATABASEURI)
 
 @app.route("/")
 def hello():
-	return render_template("index.html")
+	try:
+		engine.connect()
+	except:
+		print("couldn't connect")
+		
+	return render_template("index.html", name="john")
 
 if __name__ == "__main__":
 	app.run(debug=True)
