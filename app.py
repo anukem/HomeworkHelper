cb78b4d362c78a61757c30a23eee8e52f965b13d
janukem <janukem@due.com>
2018-04-13 02:39:05 -0400

    Added DB functionality

--- app.py
+++ app.py
@@ -3,18 +3,23 @@ from sqlalchemy.pool import NullPool
 from flask import Flask, request, render_template, g, redirect, Response
 
 app = Flask(__name__)
-DATABASEURI = "postgresql://jea2161:1955@35.227.79.146/proj1part2"
 
+# global vars for accessing DB 
+DATABASEURI = "postgresql://jea2161:1955@35.227.79.146/proj1part2"
 engine = create_engine(DATABASEURI)
 
+# connect to the DB and err out 
+engine_connection = None 
+try:
+	engine_connection = engine.connect()
+except:
+	print("couldn't connect")
+	import traceback; traceback.print_exc()
+	g.conn = None
+
 @app.route("/")
-def hello():
-	try:
-		engine.connect()
-	except:
-		print("couldn't connect")
-		
-	return render_template("index.html", name="john")
+def login():
+	return render_template("login.html")
 
 if __name__ == "__main__":
 	app.run(debug=True)
