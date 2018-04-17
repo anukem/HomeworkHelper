a8aab3fc9b32b5e3fc45c10793f7ebf14d9900f6
Braxton Gunter <braxton.gun@gmail.com>
2018-04-17 14:25:18 -0400

    add create modal

--- app/static/js/controller.js
+++ app/static/js/controller.js
@@ -1,6 +1,10 @@
+function getUserId() { // TODO
+  return '53';
+}
 
-// AJAX request: schedule per day
-function getDaySchedule(day, month, year, element, callback) {
+
+// AJAX request: schedule per day - 'api/courses'
+function getDateSchedule(date, element, callback) {
   var xhttp = new XMLHttpRequest();
   xhttp.open('POST', 'api/courses', true);
   xhttp.setRequestHeader('Content-Type', 'application/json');
@@ -17,20 +21,21 @@ function getDaySchedule(day, month, year, element, callback) {
 
   // make POST request
   xhttp.send(JSON.stringify({
-    userId: 53,
+    userId: getUserId(),
     date: {
-      day: day,
-      month: month,
-      year: year
+      day: date.day,
+      month: date.month,
+      year: date.year
     }
   }));
 }
 
-function get_all_assignments(day, month, year, element, callback){
+// AJAX request: course creation - 'api/create-course'
+function createCourse(course, element, callback) {
   var xhttp = new XMLHttpRequest();
-  xhttp.open('POST', 'api/assignments', true);
+  xhttp.open('POST', 'api/create-course', true);
   xhttp.setRequestHeader('Content-Type', 'application/json');
-
+  // data response
   xhttp.onreadystatechange = function() {
     if (xhttp.readyState == 4 && xhttp.status == 200) {
       respData = JSON.parse(xhttp.responseText);
@@ -40,12 +45,39 @@ function get_all_assignments(day, month, year, element, callback){
       return callback(err, element, respData);
     }
   };
+  // make POST request
+  xhttp.send(JSON.stringify({
+    userId: getUserId(),
+    courseName: course.name,
+    location: course.location,
+    startDate: course.startDate,
+    endDate: course.endDate,
+    startTime: course.startTime,
+    endTime: course.endTime,
+    daysOfWeek: course.daysOfWeek,
+    teacherId: course.teacherId || 31
+  }));
 
+// AJAX request: get assignments - 'api/assignments'
+function get_all_assignments(date, element, callback){
+  var xhttp = new XMLHttpRequest();
+  xhttp.open('POST', 'api/assignments', true);
+  xhttp.setRequestHeader('Content-Type', 'application/json');
+  xhttp.onreadystatechange = function() {
+    if (xhttp.readyState == 4 && xhttp.status == 200) {
+      respData = JSON.parse(xhttp.responseText);
+      return callback(null, element, respData);
+    } else if (xhttp.readyState == 4) {
+      var err = new Error(xhttp.status);
+      return callback(err, element, respData);
+    }
+  };
+  // make POST request
   xhttp.send(JSON.stringify({
     userId: 53,
     date: {
-      day: day,
-      month: month,
-      year: year
+      day: date.day,
+      month: date.month,
+      year: date.year
     }}));
 }
