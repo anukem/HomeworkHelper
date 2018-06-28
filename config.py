7ce30a32d918791923f1c8167dcb8dc2d093cb49
Braxton Gunter <braxton.gun@gmail.com>
2018-06-28 18:22:44 -0500

    config edit

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
+import os
+
 # application configurations
 DEBUG=False
-SQLALCHEMY_DATABASE_URI="postgresql://jea2161:1955@35.227.79.146/proj1part2"
+# SQLALCHEMY_DATABASE_URI="postgresql://jea2161:1955@35.227.79.146/proj1part2"
+APPNAME=os.getenv('APPNAME', 'homework_helper')
+SQLALCHEMY_DATABASE_URI="postgresql://user@127.0.0.1/%s" % APPNAME
 SQLALCHEMY_ECHO=False
 TESTING=False
 HOST='127.0.0.1'
